5a43620004b12ef492c884d3bd72926899939cd1
skerschhackl <ste.kerschhackl@gmail.com>
2023-04-18 20:10:34 +1200

    add express package

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.ts',
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
