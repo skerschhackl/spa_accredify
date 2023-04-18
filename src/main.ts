07b18cb88de213ddbfdac0f72c9a049b2b9d2048
skerschhackl <ste.kerschhackl@gmail.com>
2023-04-18 20:11:40 +1200

    fix import error for vue chart library

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+// @ts-ignore
 import Donut from 'vue-css-donut-chart';
 import 'vue-css-donut-chart/dist/vcdonut.css';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
 app.use(Donut);
 
-app.mount('#app')
+app.mount('#app');
