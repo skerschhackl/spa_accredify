b2a9c2f25292959f34f2015dd7db96ff2e02c697
skerschhackl <ste.kerschhackl@gmail.com>
2023-04-22 02:00:55 +1200

    update UtilityNavBar specs

--- src/components/__tests__/UtilityNavBar.spec.ts
+++ src/components/__tests__/UtilityNavBar.spec.ts
@@ -1,15 +1,23 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, test, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import UtilityNavBar from '../UtilityNavBar.vue'
 
-const props = { firstName: 'Mickey', lastName: 'Mouse' }
-
 describe('UtilityNavBar', () => {
-  it('shows the user initials next to the username', () => {
-    const wrapper = mount(UtilityNavBar, { props })
-
+  
+  const wrapper = mount(UtilityNavBar, { props: { name: 'Mickey Mouse' } });
+  
+  it('renders the user name next to their initials', () => {
     expect(wrapper.find('div.utility-nav-bar--initials').text()).toContain('MM')
     expect(wrapper.find('div.utility-nav-bar--username').text()).toContain('Mickey Mouse')
   })
+
+  test ('it renders no initials and no username if name is not given',async  () => {
+    await wrapper.setProps({ name: undefined });
+    
+    expect(wrapper.find('div.utility-nav-bar--initials').exists()).toBe(false); 
+    expect(wrapper.find('div.utility-nav-bar--username').exists()).toBe(false);
+  })
+  
 })
+
